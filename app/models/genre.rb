a0abf6d471aaefa3ff845543818f8d833ea26de6
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-01 18:22:46 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,7 +11,9 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists.name.select_all.to_a
+    names = []
+    self.artists.each do |artist|
+      names << artist.name
   end
 
 end
